56df671b2f07909cc6809ad1e8e639b943936b27
yatinranga <41532056+yatinranga@users.noreply.github.com>
2020-10-01 05:43:08 +0530

    Input Test begineer level

---
+++ src/begineer/EnormousInputTest.java
@@ -0,0 +1,47 @@
+package begineer;
+
+import java.util.Scanner;
+
+/**
+ * The purpose of this problem is to verify whether the method you are using to
+ * read input data is sufficiently fast to handle problems branded with the
+ * enormous Input/Output warning. You are expected to be able to process at
+ * least 2.5MB of input data per second at runtime. Input
+ * 
+ * The input begins with two positive integers n k (n, k<=107). The next n lines
+ * of input contain one positive integer ti, not greater than 109, each. Output
+ * 
+ * Write a single integer to output, denoting how many integers ti are divisible
+ * by k. Example
+ * 
+ * Input: 7 3 1 51 966369 7 9 999996 11
+ * 
+ * Output: 4
+ * 
+ * @author Anonymous
+ *
+ */
+
+public class EnormousInputTest {
+
+	public static void main(String[] args) {
+		try {
+			Scanner scan = new Scanner(System.in);
+			int n = scan.nextInt();
+			int k = scan.nextInt();
+			int count = 0;
+			while (n != 0) {
+				if (scan.nextInt() % k == 0) {
+					count++;
+				}
+				n--;
+			}
+			System.out.println(count);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
+
+}
